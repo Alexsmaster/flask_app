f1de6489aa4b3c2104894f17c957019339889f18
alexsmaster <alexsmaster@gmail.com>
2024-02-07 01:52:50 +0400

    Investigation about selecting points and redraw them in another color

--- app/static/draw_script.js
+++ app/static/draw_script.js
@@ -9,10 +9,10 @@ var trace1 = {
       y: [],
       type: 'scatter',
       mode: 'markers',
-      marker: {color: 'rgba(120, 20, 130, 1)', size: 8},
+      marker: {color: 'rgba(250, 20, 130, 1)', size: 8},
       selected: {
         marker: {
-          color: '#ff0000',
+          color: '#fff000',
           opacity: 0.8
         }
       },
@@ -44,7 +44,7 @@ var layout = {
   title: 'Responsive to window\'s size!',
   hovermode:'closest',
   font: {size: 19},
-  clickmode: 'select',
+  // clickmode: 'select',
   selected: {
     marker: {
       color: '#ff0000',
@@ -70,26 +70,27 @@ var config = {
   //   scale: 1 // Multiply title/legend/axis/canvas sizes by this factor
   // },
 
-  modeBarButtonsToAdd: [
-    {
-      name: 'color toggler',
-//      icon: icon1,
-      click: function(gd) {
-        var newColor = colors[Math.floor(3 * Math.random())]
-        Plotly.restyle(gd, 'line.color', newColor)
-      }
-    },
+//   modeBarButtonsToAdd: [
+//     {
+//       name: 'color toggler',
+// //      icon: icon1,
+//       click: function(gd) {
+//         var newColor = colors[Math.floor(3 * Math.random())]
+//         Plotly.restyle(gd, 'line.color', newColor)
+//       }
+//     },
 
-    {
-      name: 'button1',
-      icon: Plotly.Icons.pencil,
-      direction: 'up',
-      click: function(gd) {
-        alert('button1')
-      }
-    }
-  ],
-  modeBarButtonsToRemove: ['pan2d','select2d','lasso2d','resetScale2d','zoomOut2d'],
+//     {
+//       name: 'button1',
+//       icon: Plotly.Icons.pencil,
+//       direction: 'up',
+//       click: function(gd) {
+//         alert('button1')
+//       }
+//     }
+//   ],
+
+  modeBarButtonsToRemove: ['pan2d','lasso2d','resetScale2d','zoomOut2d'], //'select2d',
   displayModeBar: true,
   scrollZoom: true,
   responsive: true,
@@ -114,21 +115,35 @@ $.get("/draw/data_request", (data1, status) => {
   trace1.y = data1.points.y;
 //  Plotly.react('plot_div', data, layout);
   Plotly.newPlot('plot_div', data, layout, config);
-  myPlot.on('plotly_click', function(data){
-    var pts = '';
-    for(var i=0; i < data.points.length; i++){
-        pts = 'x = '+data.points[i].x +'\ny = '+
-            data.points[i].y.toPrecision(4) + '\n\n';
-    }
-//    alert('Closest point clicked:\n\n'+pts);
-});
+  var myPlot = document.getElementById('plot_div');
+
+  // myPlot.on('plotly_click', function(data){
+  //   var pts = '';
+  //   for(var i=0; i < data.points.length; i++){
+  //       pts = 'x = '+data.points[i].x +'\ny = '+
+  //           data.points[i].y.toPrecision(4) + '\n\n';
+  //   }
+  //  alert('Closest point clicked:\n\n'+pts);
+  // });
+
+  // myPlot.on('plotly_doubleclick', function(data){
+  //   var orgColors = ['#00000','#00000','#00000',
+  //                    '#00000','#00000','#00000'];
+  //   var update = {'marker':{color: '#00000', size:8}};
+  //   Plotly.restyle('plot_div', update);
+  // });
+  
+  myPlot.on("plotly_selected", function(eventData) {
+    var selectedPoints = eventData.points;
+    console.log(selectedPoints);
+  });
+  
+  // Programmatically trigger the selection event
+  var eventData = {points: selectedPoints};
+  
+  myPlot.emit('plotly_selected', eventData);
+
 });
 
-var myPlot = document.getElementById('plot_div');
 
-myPlot.on('plotly_doubleclick', function(data){
-  var orgColors = ['#00000','#00000','#00000',
-                   '#00000','#00000','#00000'];
-  var update = {'marker':{color: orgColors, size:16}};
-  Plotly.restyle('myDiv', update);
-});
+
