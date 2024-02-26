0735a0dd113be997be299edc5ac3360b1e18c2e2
alexsmaster <alexsmaster@gmail.com>
2024-02-26 20:07:09 +0400

    updated requrements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,20 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from app import routes, models, errors
 
-
+if __name__ == '__microblog__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='127.0.0.1', port=8000)
 
 
